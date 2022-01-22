c5469d75ee59b85e3f3b212bf03419ba6035b07b
Giuseppe Caruso <peppecaruso@gmail.com>
2022-01-22 19:16:37 +0100

    add keystone conf

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // TODO: schema items to go here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+});
