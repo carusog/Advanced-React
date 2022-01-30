a6101ea9d94b5d8a09bd46ba66f45e81f0f8db19
Giuseppe Caruso <peppecaruso@gmail.com>
2022-01-30 16:36:55 +0100

    set up Apollo Client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,33 @@
-import NProgress from 'nprogress';
+import { ApolloProvider } from '@apollo/client';
 import Router from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
-
-// Progress bar styles
+import withData from '../lib/withData';
+// Progress bar style
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
+  console.log(apollo);
+
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+App.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+}
+export default withData(App);
